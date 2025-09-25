86bd3cc795155d052e6e404d464f82b459fc72bd
TetiankaSh <shh.tetiana@gmail.com>
2025-09-25 11:59:48 +0300

    initial solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +16,82 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
-
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
-
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
-
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
-    </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
+const Good = ({ goods }) => (
+  <ul>
+    {goods.map((good, index) => (
+      <li data-cy="Good" key={index}>
+        {good}
+      </li>
+    ))}
+  </ul>
 );
+
+export const App = () => {
+  const [sorted, setSorted] = useState(goodsFromServer);
+  const [sortMode, setSortMode] = useState(null);
+
+  const sortAlphabetically = () => {
+    setSorted([...sorted].sort((good1, good2) => good1.localeCompare(good2)));
+    setSortMode('alphabetical');
+  };
+
+  const sortByLength = () => {
+    setSorted([...sorted].sort((good1, good2) => good1.length - good2.length));
+    setSortMode('length');
+  };
+
+  const reset = () => {
+    setSorted(goodsFromServer);
+    setSortMode(null);
+  };
+
+  const reverse = () => {
+    setSorted([...sorted].reverse());
+    setSortMode('reversed');
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': sortMode !== 'alphabetical',
+          })}
+          onClick={sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': sortMode !== 'length',
+          })}
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': sortMode !== 'reversed',
+          })}
+          onClick={reverse}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={reset}
+        >
+          Reset
+        </button>
+      </div>
+      <Good goods={sorted} />
+    </div>
+  );
+};
